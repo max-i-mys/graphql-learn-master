d6082d37d82519ef17d18463368ea56bc669444d
max-i-mys <maximys6205@gmail.com>
2021-11-24 17:03:52 +0200

    deleted unnecessary fields

--- src/mutations/updateUser.js
+++ src/mutations/updateUser.js
@@ -4,10 +4,7 @@ export const UPDATE_USER = gql`
 	mutation Mutation($where: users_bool_exp!, $set: users_set_input) {
 		update_users(where: $where, _set: $set) {
 			returning {
-				name
-				rocket
-				timestamp
-				twitter
+				id
 			}
 		}
 	}
